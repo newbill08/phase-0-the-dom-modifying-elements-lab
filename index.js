84c916173bf08442b68707b27d71f4386f0509ad
joshua <josh.newbill08@hotmail.com>
2023-05-05 18:06:28 -0500

    updated value

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+// grabs main and remove the tag main
+const mainpart = document.querySelector("main")
+mainpart.remove()
+
+//variable the points to node
+let newHeader = document.createElement('h1')
+
+//      make/add id of 'victory'
+newHeader.setAttribute('id','victory');
+
+//
+newHeader.textContent = 'josh is the champion'
